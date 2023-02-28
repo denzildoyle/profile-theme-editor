784cedc1d31b591306490c7ea4579e61be51cd73
Denzil Doyle <denzildoyle@gmail.com>
2023-02-27 20:24:43 -0400

    Refactor @input @output

--- src/app/edit/edit.component.ts
+++ src/app/edit/edit.component.ts
@@ -13,93 +13,17 @@ export class EditComponent {
     this.show = !this.show;
   }
 
-
-  //profile
-
-  @Input() public name!: string;
-  @Output() nameChange = new EventEmitter<string>();
+  @Input() public user!: any;
+  @Output() userChange = new EventEmitter<any>();
   
-  changeName(name: string){
-    this.nameChange.emit(name);
+  changeUser(){
+    this.userChange.emit(this.user);
   }
 
-  @Input() public bio!: string;
-  @Output() bioChange = new EventEmitter<string>();
+  @Input() public theme!: any;
+  @Output() themeChange = new EventEmitter<any>();
   
-  changeBio(bio: string){
-    this.bioChange.emit(bio);
-  }
-
-  @Input() public twitter!: string;
-  @Output() twitterChange = new EventEmitter<string>();
-  
-  changeTwitter(twitter: string){
-    this.twitterChange.emit(twitter);
-  }
-
-  @Input() public instagram!: string;
-  @Output() instagramChange = new EventEmitter<string>();
-  
-  changeInstagram(instagram: string){
-    this.instagramChange.emit(instagram);
-  }
-
-  @Input() public linkedin!: string;
-  @Output() linkedinChange = new EventEmitter<string>();
-  
-  changeLinkedin(linkedin: string){
-    this.linkedinChange.emit(linkedin);
-  }
-
-  @Input() public link!: string;
-  @Output() linkChange = new EventEmitter<string>();
-  
-  changeLink(link: string){
-    this.linkChange.emit(link);
-  }
-
-
-  //theme
-
-  @Input() public nameColor!: string;
-  @Output() nameColorChange = new EventEmitter<string>();
-  
-  changeNameColor(nameColor: string){
-    this.nameColorChange.emit(nameColor);
-  }
-
-  @Input() public avatarShape!: string;
-  @Output() avatarShapeChange = new EventEmitter<string>();
-  
-  changeAvatarShape(avatarShape: string){
-    this.avatarShapeChange.emit(avatarShape);
-  }
-
-  @Input() public bioColor!: string;
-  @Output() bioColorChange = new EventEmitter<string>();
-  
-  changeBioColor(bioColor: string){
-    this.bioColorChange.emit(bioColor);
-  }
-
-  @Input() public linkBRadius!: number;
-  @Output() linkBRadiusChange = new EventEmitter<number>();
-  
-  changeLinkBRadius(linkBRadius: number){
-    this.linkBRadiusChange.emit(linkBRadius);
-  }
-
-  @Input() public linkBGColor!: string;
-  @Output() linkBGColorChange = new EventEmitter<string>();
-  
-  changeLinkBGColor(linkBGColor: string){
-    this.linkBGColorChange.emit(linkBGColor);
-  }
-
-  @Input() public linkColor!: string;
-  @Output() linkColorChange = new EventEmitter<string>();
-  
-  changeLinkColor(linkColor: string){
-    this.linkColorChange.emit(linkColor);
+  changeTheme(){
+    this.themeChange.emit(this.theme);
   }
 }
